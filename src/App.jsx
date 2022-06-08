0b2ba1402d5739c49ccafd9fca7841e6b06f2d45
XU LU <lutarring@gmail.com>
2022-06-08 17:06:22 +0900

    updated firebase hosting

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,26 @@ import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const App = () => {
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyClzUAdiEkAYGqLf8yyq4t8Wq1CIi1IOVQ",
+  authDomain: "react-portfolio-b55f5.firebaseapp.com",
+  projectId: "react-portfolio-b55f5",
+  storageBucket: "react-portfolio-b55f5.appspot.com",
+  messagingSenderId: "630712427374",
+  appId: "1:630712427374:web:184115ef755d126911177f",
+  measurementId: "G-4GWK86W3EV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
   return (
     <>
      <Header />
--- src/components/header/ContactButton.jsx
+++ src/components/header/ContactButton.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/CV.pdf'
 const ContactButton = () => {
   return (
     <div className="contact-button">
-    　　<a href={CV} download className="btn">Download CV</a>
+    <a href={CV} download className="btn">Download CV</a>
     <a href="#contactme" className="btn btn-primary">Contact Me!</a>
       </div>
 
